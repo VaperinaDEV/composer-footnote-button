c9e63f102ce9aea5f7a4da8e9b0fd333e0c450a3
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-10-30 19:40:26 +0100

    Add files via upload

---
+++ javascripts/discourse/composer-footnote-button/initializers/composer-footnote-button.js
@@ -0,0 +1,26 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+
+export default {
+  name: "composer-footnote-button",
+
+  initialize() {
+    withPluginApi("0.8", (api) => {
+      api.onToolbarCreate(function(toolbar) {
+        toolbar.addButton({
+          trimLeading: true,
+          id: "quick-footnote",
+          group: "extras",
+          icon: 'superscript',
+          title: "composer_footnote_button.title",
+          perform: function(e) {
+            return e.applySurround(
+              '^[',
+              "]",
+              "composer_footnote_button.text"
+            );
+          }
+        });
+      });
+    });
+  },
+};
