3b5aded20930bc40b5c7f77c19845e188e0a0614
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-10-31 10:37:58 +0100

    Update inlines-footnotes.js

--- javascripts/discourse/composer-footnote-button/initializers/inlines-footnotes.js
+++ javascripts/discourse/composer-footnote-button/initializers/inlines-footnotes.js
@@ -52,8 +52,8 @@ function footNoteEventHandler(event) {
   tooltip?.removeAttribute("data-show");
 
   // if we didn't actually click a footnote button, exit early
-  if (event.target.classList.contains("expand-footnote")) {
-    return;
+  if (!event.target.classList.contains("expand-footnote")) {
+    return false;
   }
 
   event.preventDefault();
