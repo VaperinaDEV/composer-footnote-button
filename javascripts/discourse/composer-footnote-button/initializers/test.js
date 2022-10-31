029335afba8ca51724687e460fbdf4d81cdbb8f7
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-10-31 12:04:52 +0100

    Update test.js

--- javascripts/discourse/composer-footnote-button/initializers/test.js
+++ javascripts/discourse/composer-footnote-button/initializers/test.js
@@ -49,10 +49,14 @@ function footNoteEventHandler(event) {
 
   // reset state by hidding tooltip, it handles "click outside"
   // allowing to hide the tooltip when you click anywhere else
-  tooltip?.removeAttribute("");
+  tooltip?.removeAttribute("data-show");
 
   // if we didn't actually click a footnote button, exit early
   if (!event.target.classList.contains("expand-footnote")) {
+    return;
+  }
+  
+  if (!event.target.classList.contains("footnote-tooltip-content")) {
     return;
   }
 
